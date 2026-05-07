246c730fb603f61e20e81b45e846e57e60352089
sukhpreetop248-star <sukhpreetop248@gmail.com>
2026-05-07 16:35:52 +0530

    Create index.js
    const { Client, GatewayIntentBits } = require('discord.js');
    
    const client = new Client({
      intents: [GatewayIntentBits.Guilds]
    });
    
    client.once('ready', () => {
      console.log(`${client.user.tag} online!`);
    });
    
    client.login(process.env.TOKEN);

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} online!`);
+});
+
+client.login(process.env.TOKEN);
