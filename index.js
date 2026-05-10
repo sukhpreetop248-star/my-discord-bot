d8a0af31b9aac38486b15caaecd2bd4e6870f7ce
sukhpreetop248-star <sukhpreetop248@gmail.com>
2026-05-10 10:23:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,3 +57,14 @@ client.on(Events.InteractionCreate, async interaction => {
 });
 
 client.login(process.env.TOKEN);
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} online!`);
+});
+
+client.login(process.env.TOKEN);
